461b2aeb94395bf8d02599c9565d60d8ef470c9c
Benjamin Stürz <benni@stuerz.xyz>
2025-03-13 17:49:16 +0000

    configure: remove tabs before preprocessor directives

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,10 @@ export AUTOCONF_VERSION
 
 autoconf
 autoheader
-sed 's/defined (\{0,1\}\([a-zA-Z0-9_$$]\{1,\}\))\{0,1\}/defined(\1)/' configure > configure.new
+sed	-e 's/defined (\{0,1\}\([a-zA-Z0-9_$$]\{1,\}\))\{0,1\}/defined(\1)/'	\
+	-e 's/^[[:space:]]*#/#/'							\
+	configure > configure.new
 mv configure.new configure
 chmod +x configure
 rm -rf autom4te.cache
+
