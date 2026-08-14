aa4ea55d3bbbb2b3503b42b473558de94ce63912
Benjamin Stürz <benni@stuerz.xyz>
2026-08-14 15:23:49 +0000

    document data structures more

--- mk.h
+++ mk.h
@@ -7,14 +7,14 @@ enum path_type {
 	PATH_NAME,
 };
 struct path {
-	enum path_type type;
-	char *name;
+	enum path_type		 type;
+	char			*name;
 };
 
 struct template {
-	SLIST_ENTRY(template) next;
-	char *name;
-	char *text;
+	SLIST_ENTRY(template)	 next;
+	char			*name;
+	char			*text;
 };
 SLIST_HEAD(template_list, template);
 
@@ -24,15 +24,15 @@ enum scope_type {
 };
 
 struct scope {
-	SLIST_ENTRY(scope) next;
-	enum scope_type type;
-	char *name; /* optional */
-	struct scope *parent; /* optional */
-	char *makefile; /* required */
-	int created;
+	SLIST_ENTRY(scope)	 next;
+	enum scope_type		 type;
+	char			*name;		/* optional */
+	struct scope		*parent;	/* optional */
+	char			*makefile;	/* required */
+	int			 created;	/* mkdir() */
 	union {
-		struct directory *dir; /* optional */
-		struct foreign *foreign; /* required */
+		struct directory	*dir;	/* optional */
+		struct foreign		*foreign; /* required */
 	} inner;
 };
 SLIST_HEAD(scope_list, scope);
@@ -42,39 +42,41 @@ SLIST_HEAD(scope_list, scope);
  * struct foreign (via dhead/dtail).  Ownership is expressed as a TAILQ.
  */
 struct dep {
-	TAILQ_ENTRY(dep) link;
-	struct path *path;
-	int obj;
+	TAILQ_ENTRY(dep)	 link;
+	struct path		*path;
+	int			 obj;
 };
 TAILQ_HEAD(dep_list, dep);
 
 enum file_state {
-	FILE_PENDING,
-	FILE_BUSY,
-	FILE_DONE,
+	FILE_PENDING,	/* not yet started */
+	FILE_BUSY,	/* currently building */
+	FILE_DONE,	/* built */
 };
 
 /*
  * struct file: doubly-linked, owned by struct directory (fhead/ftail).
  */
 struct file {
-	TAILQ_ENTRY(file) link;
-	char *name;
-	struct rule *rule; /* optional */
-	struct dep_list deps;
-	struct inference *inf; /* optional */
-	struct timespec mtime;
-	char *help; /* optional */
-	int obj, err;
-	enum file_state state;
+	TAILQ_ENTRY(file)	 link;
+	char			*name;
+	struct rule		*rule;	/* optional */
+	struct dep_list		 deps;
+	struct inference	*inf;	/* optional */
+	struct timespec		 mtime;
+	char			*help;	/* optional */
+	int			 obj;
+	int			 err;
+	enum file_state		 state;
 };
 TAILQ_HEAD(file_list, file);
 
 struct inference {
-	SLIST_ENTRY(inference) next;
-	char *from, *to;
-	struct rule *rule;
-	struct dep_list deps;
+	SLIST_ENTRY(inference)	 next;
+	char			*from;
+	char			*to;
+	struct rule		*rule;
+	struct dep_list		 deps;
 };
 SLIST_HEAD(inference_list, inference);
 
@@ -86,39 +88,40 @@ SLIST_HEAD(inference_list, inference);
  * is prepended (+=) to this one's value at expansion time.
  */
 struct macro {
-	SLIST_ENTRY(macro) next;
-	SLIST_ENTRY(macro) enext;
-	struct macro *prepend;
-	char *name; /* required */
-	char *value; /* required */
-	char *help; /* optional */
-	int lazy;
+	SLIST_ENTRY(macro)	 next;
+	SLIST_ENTRY(macro)	 enext;
+	struct macro		*prepend;	/* prepend the value of this macro (+=) (optional) */
+	char			*name;		/* required */
+	char			*value;		/* required */
+	char			*help;		/* optional */
+	int			 lazy;		/* the value is not yet expanded */
 };
 SLIST_HEAD(macro_list, macro);
 
 struct directory {
-	struct scope_list subdirs;
-	struct file_list files;
-	struct macro_list macros;
-	struct macro_list emacros;	/* exported macros */
-	struct inference_list infs;
-	struct template_list templates;
-	char *default_file;
-	int done;
+	struct scope_list	 subdirs;	/* sub directories list */
+	struct file_list	 files;		/* file list */
+	struct macro_list	 macros;	/* macro list */
+	struct macro_list	 emacros;	/* exported macros list */
+	struct inference_list	 infs;		/* inference rules */
+	struct template_list	 templates;	/* list of templates */
+	char			*default_file;	/* default makefile name */
+	int			 done;		/* directory makefile is parsed */
 };
 
 struct cbuilt {
-	SLIST_ENTRY(cbuilt) next;
-	char *name;
-	struct timespec t;
-	int obj;
+	SLIST_ENTRY(cbuilt)	 next;
+	char			*name;
+	struct timespec		 t;
+	int			 obj;
 };
 SLIST_HEAD(cbuilt_list, cbuilt);
 
 struct foreign {
-	struct file *test, *exec;
-	struct dep_list deps; /* ordering deps (bare name: target) */
-	struct cbuilt_list built; /* targets already built this run */
+	struct file		*test;	/* rule to test if a target is up-to-date (optional) */
+	struct file		*exec;	/* rule to build a target */
+	struct dep_list		 deps;	/* ordering deps (bare name: target) */
+	struct cbuilt_list	 built;	/* targets already built this run */
 };
 
 struct rule {
